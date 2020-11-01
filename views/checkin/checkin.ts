67aa46f5984f3524b6f4465b7be9e83c69354ae9
damr <damr18@gmail.com>
2020-11-01 09:18:52 -0600

    ingresar temp valida

---
+++ views/checkin/checkin.ts
@@ -0,0 +1,26 @@
+
+const {fromObject} = require("@nativescript/core")
+
+const obj = fromObject({
+    temp: ''
+})
+
+var page;
+
+
+
+exports.loaded = function(args) {
+    page = args.object;  
+    page.bindingContext = obj
+}
+
+exports.onSubmit = function(args) {
+    var temperature = parseFloat(obj.get('temp'))
+    if (temperature<35 || temperature>40 || isNaN(temperature) ){
+        alert('temperatura no valida (35 - 40°)')
+    }else{
+        alert('Enviado! '+ temperature)
+    }
+    
+
+}
