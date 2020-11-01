3806a60a9800b27f1c3e88e8e4727787f97508ff
damr <damr18@gmail.com>
2020-11-01 13:26:40 -0600

    escanear codigo e ingresar temp

--- views/checkin/checkin.ts
+++ views/checkin/checkin.ts
@@ -13,11 +13,16 @@ exports.loaded = function(args) {
     page = args.object;  
     page.bindingContext = obj
 }
+exports.onNavigatedTo = function(args) {
+    const context = args.context;  
+    alert(context.data)
+}
+
 
 exports.onSubmit = function(args) {
     var temperature = parseFloat(obj.get('temp'))
     if (temperature<35 || temperature>40 || isNaN(temperature) ){
-        alert('temperatura no valida (35 - 40°)')
+        alert('temperatura no valida (35° - 40°)')
     }else{
         alert('Enviado! '+ temperature)
     }
--- views/home/home.ts
+++ views/home/home.ts
@@ -1,7 +1,24 @@
 // import barcodescannerModule  = require("nativescript-barcodescanner");
 import { BarcodeScanner  } from "nativescript-barcodescanner";
 let barcodescannerModule = new BarcodeScanner();
+const {fromObject} = require("@nativescript/core")
 
+const obj = fromObject({
+    temp: '',
+    clave:''
+})
+
+var clave;
+var page;
+ 
+
+export function loaded(args) {
+    page = args.object;  
+    page.bindingContext = obj
+
+
+   
+}
 
 export function requestPermission() {
     return new Promise((resolve, reject) => {
@@ -23,20 +40,49 @@ export function requestPermission() {
     });
 }
 
+ 
+export function onSubmit(){
+    var temperature = parseFloat(obj.get('temp'))
+    if (temperature<35 || temperature>40 || isNaN(temperature) ){
+        alert('temperatura no valida (35° - 40°)')
+    }else{
+        alert('Enviado! '+ temperature)
+    }
+    
+
+}
+
 export function scanBarcode() {
-    requestPermission().then((result) => {
-        barcodescannerModule.scan({
-            cancelLabel: "Stop scanning",
-            message: "Go scan something",
-            preferFrontCamera: false,
-            showFlipCameraButton: true
-        }).then((result) => {
-            console.log("Scan format: " + result.format);
-            console.log("Scan text:   " + result.text);
-        }, (error) => {
-            console.log("No scan: " + error);
-        });
-    }, (error) => {
-        console.log("ERROR", error);
-    });
+    //  clave = new Promise((resolve,reject) =>{
+        requestPermission().then((result) => {
+            barcodescannerModule.scan({
+                   cancelLabel: "Stop scanning",
+                   message: "Go scan something",
+                   preferFrontCamera: false,
+                   showFlipCameraButton: true
+               }).then((result) => {
+                   console.log("Scan format: " + result.format);
+                   console.log("Scan text:   " + result.text);                                      
+                   obj.set('clave',result.text)
+                //    page.bindingContext = {      
+                //     clave:result.text       
+                //   }
+                
+                //    resolve(result.text)
+
+               }, (error) => {
+                   console.log("No scan: " + error);
+                //    reject(error)
+               });
+           }, (error) => {
+               console.log("ERROR", error);
+            //    reject(error)
+           });
+    // })
+
+  
+   
+ 
+
+ 
 }
