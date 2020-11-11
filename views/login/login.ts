dcf0f77c7a8479445630c66e524ccd481d68baec
damr <damr18@gmail.com>
2020-11-11 10:54:03 -0600

    experimenting with picker

--- views/login/login.ts
+++ views/login/login.ts
@@ -2,14 +2,16 @@ const {fromObject} = require("@nativescript/core")
 const httpModule = require("tns-core-modules/http");
 var appSettings = require("tns-core-modules/application-settings");
 
+
 const obj = fromObject({
     username:'',
     pwd:'',
-
+    years:'',
 })
 
 var page;
  
+var items = [];
 
 export function loaded(args) {
     page = args.object;  
@@ -19,36 +21,46 @@ export function loaded(args) {
     
 }
 
+export function onListPickerLoaded(fargs) {
+    const listPickerComponent = fargs.object;
+    listPickerComponent.on("selectedIndexChange", (args) => {
+        const picker = args.object;
+        console.log(`index: ${picker.selectedIndex}; item" ${items[picker.selectedIndex]}`);
+    });
+}
+
+
 
 export function onSubmit(){
     // console.log(obj.get('username') + '  ' +obj.get('pwd'))
 
-    httpModule.request({
-        url: "https://www.covidcinvestav.com/index.php?r=api/loginnegocio",
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        content: JSON.stringify({
+    // httpModule.request({
+    //     url: "https://www.covidcinvestav.com/index.php?r=api/loginnegocio",
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     content: JSON.stringify({
             
-            "username" :"damr18",
-            "pwd" : "django123"
-            // "username" :"abelnevar3",
-            // "pwd" : "mad23241"            
+    //         "username" :"damr18",
+    //         "pwd" : "django123"
+    //         // "username" :"abelnevar3",
+    //         // "pwd" : "mad23241"            
 
-        })
-    }).then((response) => {
-        var result = response.content.toJSON();
-        // alert(result[0].nombre)
-        appSettings.setString("LoggedIn","Si");
-        appSettings.setString("nombreNegocio", result[0].nombre)
-        const options1 = {
-            moduleName:"views/menu/menu",
-            clearHistory:true
-        }
-        page.frame.navigate(options1);
+    //     })
+    // }).then((response) => {
+    //     var result = response.content.toJSON();
+    //     // alert(result[0].nombre)
+    //     appSettings.setString("LoggedIn","Si");
+    //     appSettings.setString("nombreNegocio", result[0].nombre)
+    //     const options1 = {
+    //         moduleName:"views/menu/menu",
+    //         clearHistory:true
+    //     }
+    //     page.frame.navigate(options1);
         
-    }).catch((e) => {
-          console.log(e);
-      }) 
-
+    // }).catch((e) => {
+    //       console.log(e);
+    //   }) 
+    items = [1993,1994,1995,1996]
+    obj.set('years',items)
 
 }
